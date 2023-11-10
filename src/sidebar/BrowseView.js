c1fe7c4079e64c2e5e2cc59b1c6442100e5f7b21
Sonny Piers <sonny@fastmail.net>
2023-11-10 18:53:09 +0100

    Use platform instead of sdk (#13)

--- src/sidebar/BrowseView.js
+++ src/sidebar/BrowseView.js
@@ -141,13 +141,12 @@ class BrowseView extends Gtk.ScrolledWindow {
   }
 
   async #loadDocs() {
-    return Promise.all([
-      this.#scanLibraries(Gio.File.new_for_path("/usr/share/doc")),
-      this.#scanLibraries(Gio.File.new_for_path("/usr/share/gtk-doc/html")),
+    await Promise.all([
+      this.#scanLibraries(Gio.File.new_for_path("/app/share/doc/html")),
       this.#scanLibraries(Gio.File.new_for_path("/app/share/doc")),
-    ]).then(() => {
-      this.emit("browse-view-loaded");
-    });
+    ]);
+
+    this.emit("browse-view-loaded");
   }
 
   async #scanLibraries(base_dir) {
