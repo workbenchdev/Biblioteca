3f4f8aad851b1c94c5cb27bee41e367ae7a7f8ad
Sonny Piers <sonny@fastmail.net>
2023-11-18 13:20:06 +0100

    Set language to English (#41)
    So that the about dialog and other components are not translated.
    
    And remove a bunch of dead code.

--- src/about.js
+++ src/about.js
@@ -1,29 +1,8 @@
 import Gtk from "gi://Gtk";
 import { gettext as _ } from "gettext";
-import GLib from "gi://GLib";
 import Adw from "gi://Adw";
 
-import {
-  getGIRepositoryVersion,
-  getGjsVersion,
-  getGLibVersion,
-} from "../troll/src/util.js";
-import { getFlatpakInfo } from "./util.js";
-
 export default function About({ application }) {
-  const flatpak_info = getFlatpakInfo();
-
-  const debug_info = `
-${pkg.name} ${pkg.version}
-${GLib.get_os_info("ID")} ${GLib.get_os_info("VERSION_ID")}
-
-GJS ${getGjsVersion()}
-Adw ${getGIRepositoryVersion(Adw)}
-GTK ${getGIRepositoryVersion(Gtk)}
-GLib ${getGLibVersion()}
-Flatpak ${flatpak_info.get_string("Instance", "flatpak-version")}
-`.trim();
-
   const dialog = new Adw.AboutWindow({
     transient_for: application.get_active_window(),
     application_name: "Biblioteca",
@@ -34,9 +13,6 @@ Flatpak ${flatpak_info.get_string("Instance", "flatpak-version")}
     website: "https://github.com/workbenchdev/Biblioteca",
     application_icon: pkg.name,
     issue_url: "https://github.com/workbenchdev/Biblioteca/issues",
-    // TRANSLATORS: eg. 'Translator Name <your.email@domain.com>' or 'Translator Name https://website.example'
-    translator_credits: _("translator-credits"),
-    debug_info,
     developers: [
       "Akshay Warrier https://github.com/AkshayWarrier",
       "Sonny Piers https://sonny.re",
--- src/util.js
+++ src/util.js
@@ -1,114 +1,14 @@
 import GLib from "gi://GLib";
 import Gio from "gi://Gio";
-// import Xdp from "gi://Xdp";
-import GObject from "gi://GObject";
-
-// export const portal = new Xdp.Portal();
 
 export const settings = new Gio.Settings({
   schema_id: pkg.name,
   path: "/app/drey/Biblioteca/",
 });
 
-export const data_dir = Gio.File.new_for_path(
-  GLib.build_filenamev([GLib.get_user_data_dir(), pkg.name]),
-);
-
-export function ensureDir(file) {
-  try {
-    file.make_directory_with_parents(null);
-  } catch (err) {
-    if (!err.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.EXISTS)) {
-      throw err;
-    }
-  }
-}
-
-export function getFlatpakInfo() {
-  const keyFile = new GLib.KeyFile();
-  try {
-    keyFile.load_from_file("/.flatpak-info", GLib.KeyFileFlags.NONE);
-  } catch (err) {
-    if (!err.matches(GLib.FileError, GLib.FileError.NOENT)) {
-      console.error(err);
-    }
-    return null;
-  }
-  return keyFile;
-}
-
-export function listenProperty(object, property, fn, { initial = false } = {}) {
-  if (initial) {
-    fn(object[property]);
-  }
-
-  const signal = `notify::${property}`;
-  const handler_id = object.connect(signal, () => {
-    fn(object[property]);
-  });
-  return {
-    block() {
-      GObject.signal_handler_block(object, handler_id);
-    },
-    unblock() {
-      GObject.signal_handler_unblock(object, handler_id);
-    },
-    disconnect() {
-      return object.disconnect(handler_id);
-    },
-  };
-}
-
 export function decode(data) {
   if (data instanceof GLib.Bytes) {
     data = data.toArray();
   }
   return new TextDecoder().decode(data);
-}
-
-export function encode(data) {
-  return new TextEncoder().encode(
-    data.to_string?.() || data?.toString?.() || data,
-  );
-}
-
-// Take a function that return a promise and returns a function
-// that will discard all calls during a pending execution
-// it's like a job queue with a max size of 1 and no concurrency
-export function unstack(fn, onError = console.error) {
-  let latest_promise;
-  let latest_arguments;
-  let pending = false;
-
-  return function unstack_wrapper(...args) {
-    latest_arguments = args;
-
-    if (pending) return;
-
-    if (!latest_promise)
-      latest_promise = fn(...latest_arguments).catch(onError);
-
-    pending = true;
-    latest_promise.finally(() => {
-      pending = false;
-      latest_promise = fn(...latest_arguments).catch(onError);
-    });
-  };
-}
-
-export function getNowForFilename() {
-  return new GLib.DateTime().format("%Y-%m-%d %H-%M-%S");
-}
-
-// https://gitlab.gnome.org/GNOME/libadwaita/-/issues/746
-export function makeDropdownFlat(dropdown) {
-  dropdown.get_first_child().add_css_class("flat");
-}
-
-export function buildRuntimePath(...args) {
-  return GLib.build_filenamev([
-    GLib.getenv("XDG_RUNTIME_DIR"),
-    GLib.getenv("FLATPAK_ID"),
-    ...args,
-  ]);
 }
